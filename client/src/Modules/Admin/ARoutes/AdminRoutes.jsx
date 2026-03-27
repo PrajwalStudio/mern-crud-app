e4c63e311973f0d2820582ea95903a1109a30c7c
PrajwalStudio <138191549+PrajwalStudio@users.noreply.github.com>
2026-03-27 15:42:27 +0530

    Add Sidebar component and refactor ViewUser to fetch user data

--- client/src/Modules/Admin/ARoutes/AdminRoutes.jsx
+++ client/src/Modules/Admin/ARoutes/AdminRoutes.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import {Route,Routes} from 'react-router-dom'
 import AdminHome from '../AComponents/AdminHome'
 import ViewUser from '../AComponents/ViewUser'
+import Sidebar from '../AComponents/Sidebar'
 export default function AdminRoutes() {
   return (
-    <div>
+    <Sidebar>
       <Routes>
         <Route path='/' element={<AdminHome/>}/>
         <Route path='/ViewUser' element={<ViewUser/>}/>
       </Routes>
-    </div>
+    </Sidebar>
   )
 }
